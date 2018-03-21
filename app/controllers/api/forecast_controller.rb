b661857685012ea474ba588ca3673dcc6859c56a
Imre Raudsepp <justfunest@gmail.com>
2018-03-21 18:16:27 +0200

    added forecast to swagger

--- app/controllers/api/forecast_controller.rb
+++ app/controllers/api/forecast_controller.rb
@@ -2,9 +2,16 @@ module Api
   class ForecastController < BaseController
       def index
         forecast_days = 7
-        query_end_date = Time.now.to_date.to_datetime
-        query_start_date = query_end_date - forecast_days.days
+        query_start_date = Time.now.to_date.to_datetime
+        query_end_date = query_start_date + forecast_days.days
         weathers = CityWeather.where(["city_id = ? and time_of_data_forecasted BETWEEN ? AND ? ", params['city_id'], query_start_date, query_end_date])
+        if params[:limit]
+          unless (params[:limit].to_i > 0)
+            render json: {status: 'error', message: 'bad input parameter'}, status: :bad_request and return
+          else
+            weathers = weathers.limit(params[:limit])
+          end
+        end
 
         render json: weathers
       end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get '/docs' => redirect('/swagger/dist/index.html?url=/apidocs/api-docs.yaml')
+  get '/' => redirect('/swagger/dist/index.html?url=/apidocs/api-docs.yaml')
 
   namespace :api do
     resources :weather_conditions, :only => [:index, :create, :show]
